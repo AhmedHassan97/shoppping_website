b5776561d1117c984a72ff9775548fc1c675daaa
Ahmed M. Hassan <33345323+AhmedHassan97@users.noreply.github.com>
2022-01-29 14:07:14 +0200

    feat: :sparkles: add talwind

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./app/components/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {}
+  },
+  mode: "jit",
+  plugins: []
+};
