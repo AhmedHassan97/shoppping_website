e44e7359c58832df71a385b59819d0236b291ffe
Ahmed M. Hassan <33345323+AhmedHassan97@users.noreply.github.com>
2022-01-29 14:16:17 +0200

    feat: add Raleway font

---
+++ pages/_document.tsx
@@ -0,0 +1,49 @@
+// eslint-disable-next-line @next/next/no-document-import-in-page
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <meta name="theme-color" content="#FFFFFF" />
+
+          <meta name="application-name" content="Store Websiite" />
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <meta
+            name="apple-mobile-web-app-status-bar-style"
+            content="default"
+          />
+          <meta name="apple-mobile-web-app-title" content="Store Websiite" />
+          <meta name="description" content="This is Store Website" />
+          <meta name="format-detection" content="telephone=no" />
+          <meta name="mobile-web-app-capable" content="yes" />
+          {/* <link
+            rel="apple-touch-icon"
+            sizes="192x192"
+            href="icon-192x192.png"
+          />
+
+          <link
+            rel="apple-touch-icon"
+            sizes="192x192"
+            href="/icon-192x192.png"
+          />
+          <link rel="manifest" href="/manifest.json" />
+          <link rel="shortcut icon" href="/favicon.ico" /> */}
+
+          <link
+            href="https://fonts.googleapis.com/css2?family=Raleway:wght@500&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
